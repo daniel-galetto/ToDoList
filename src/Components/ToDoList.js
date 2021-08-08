36cc1efc5edc48bbd22d442e16211eee77b3a707
Daniel Galetto <d.galetto@itecriocuarto.org.ar>
2021-08-08 20:35:27 -0300

    first version whitout css

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,30 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
-//+ Libreria que genera id
 import uniqid from 'uniqid'
 
 
 const ToDoList = () => {
     
-    //+ optiene los valores que ingresan en el input
     const [task,setTask] = useState('')
-    //+ guarda en una list los valores ingresados en el input
     const [list,setList] = useState([])
-    //+ edita los contenidos de la list 
     const [edit,setEditTask] = useState(false)
     const [id,setId] = useState('')
 
     const addTask = (event)=> {
-        //+ evita que se pierdan datos cuando se recarga la pagina
         event.preventDefault()
-        //+ genera un objeto con un ID y la task
         const newTask = {
             id:uniqid(),
             taskName: task
         }
-        //+ agrega el objeto ( task y iD ) en la list
         setList([...list,newTask])
-        //+ hace que el input se borre el ultimo valor ingresado despues de un submit hay que Add  value = {task} en el input para que funcione
         setTask('')
     }
 
@@ -61,15 +53,23 @@ const ToDoList = () => {
                     <input className="btn btn-primary block m-3" type="submit" value={edit? "edit" : "Add"}/>
                 </form>
             </div>
-            <div className="row">
+            <div className="row bg-danger">
                 <h2>To Do</h2>  
                 <ul>
-                    {
-                        //+ con el metodo map recorremos los items que hay en la list y se genera un li por cada uno     
+                    {     
                         list.map(item => 
-                            <li key={item.id}  className="list-group-item">{item.taskName}
-                                <button onClick= {()=> {deleteTask(item.id)}} className="btn btn-danger float-end">Delete</button>
-                                <button onClick= {()=> {editTask(item)}} className="btn btn-warning float-end">edit</button>
+                            <li key={item.id}  className="list-group-item mt-2 p-4 bg-dark text-light">{item.taskName}
+            
+                                <button onClick= {()=> {deleteTask(item.id)}} className="btn btn-danger float-end "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>
+                                </button>
+                          
+                                <button onClick= {()=> {editTask(item)}} className="btn btn-warning float-end "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg>
+                                </button>
+
+                     
+                       
                             </li>
                         )
                     }
